9a64e6f3319a2c412b8d1a606e5e17e6b81f4af1
zuoyuan <zuoyuanlxz@gmail.com>
2016-03-31 12:47:25 +0800

    每日自动提交测试

--- submit.bat
+++ submit.bat
@@ -1,7 +1,8 @@
-@if not exist "%HOME%" @set HOME=%SystemDrive%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
 @cd /D %~dp0%
 git add .
 git commit -am %1
 git pull origin master
 git push origin master
-pause
+
